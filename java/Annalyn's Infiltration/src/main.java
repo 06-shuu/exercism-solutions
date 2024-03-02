1434de95ba5f416dfd095ded8d8b72d0bda6f39a
06-shuu <154021913+06-shuu@users.noreply.github.com>
2024-03-02 23:33:51 +0300

    Annalyn's inf. exercise - done

---
+++ java/Annalyn's Infiltration/src/main.java
@@ -0,0 +1,13 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		AnnalynsInfiltration a = new AnnalynsInfiltration();
+		System.out.println(a.canFastAttack(true));
+		System.out.println(a.canSpy(true, false, false));
+		System.out.println(a.canSignalPrisoner(false, false));
+		System.out.println(a.canFreePrisoner(true, true, false, false));
+
+	}
+
+}
