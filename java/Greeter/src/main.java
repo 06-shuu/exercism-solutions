f0d76a1cf4ebf76dbedf4c56a567a51c2969ae7f
06-shuu <154021913+06-shuu@users.noreply.github.com>
2024-03-02 23:08:07 +0300

    exercise 1 - done :)

---
+++ java/Greeter/src/main.java
@@ -0,0 +1,13 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		System.out.println(getGreeting());
+		
+	}//end of main
+
+	static String getGreeting() {
+        return "Hello, World!";
+    }
+}
