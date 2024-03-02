9e98ce601f42ae3282fbb937b96861ff23cb2bd2
06-shuu <154021913+06-shuu@users.noreply.github.com>
2024-03-03 00:08:42 +0300

    Space Age exercise - done

---
+++ java/Space Age/src/main.java
@@ -0,0 +1,19 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpaceAge sa = new SpaceAge(30);
+
+		System.out.println(sa.onEarth());
+		System.out.println(sa.onMercury());
+		System.out.println(sa.onVenus());
+		System.out.println(sa.onMars());
+		System.out.println(sa.onJupiter());
+		System.out.println(sa.onSaturn());
+		System.out.println(sa.onUranus());
+		System.out.println(sa.onNeptune());
+
+	}
+
+}
