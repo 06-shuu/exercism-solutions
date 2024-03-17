3b352bd45248f9f5cc45abc3433e05df419f0b8d
06-shuu <154021913+06-shuu@users.noreply.github.com>
2024-03-17 21:44:48 +0300

    Squeaky Clean

--- java/Squeaky Clean/src/main.java
+++ java/Squeaky Clean/src/main.java
@@ -4,7 +4,7 @@ public class main {
 	public static void main(String[] args) {
 
 		
-		System.out.println(clean("a Bc"));
+		System.out.println(clean("H3ll0#%W0rld"));
 //		String name = "shu-mokh";
 //		String dashless = name.replaceAll("\\-", "");
 //		System.out.println("dashless: " + dashless);
@@ -20,6 +20,9 @@ public class main {
 		 
 		 for (char character : identifier.toCharArray()) {
 			 //task1:
+			 if(Character.isLetter(character) || Character.isDigit(character) || Character.isWhitespace(character) || character == '-') {
+				 
+			 
 			 if(Character.isWhitespace(character) == true)
 				 word.append("_");
 			 //task2
@@ -29,10 +32,21 @@ public class main {
 	            }
 			 else if (isCap) 
 				 word.append(Character.toUpperCase(character));
+			 //task3
+			 else if(character =='0')
+				 word.append('o');
+			 else if(character == '1')
+				 word.append('l');
+			 else if(character == '3')
+				 word.append('e');
+			 else if(character == '4')
+				 word.append('a');
+			 else if(character == '7')
+				 word.append('t');
 			 else //last statement
 	             word.append(character); 
-			 
 	            isCap = false;
+			 }
 	        
 		 }//end of for each
 
