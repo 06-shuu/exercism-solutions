b3a9c60a2cc3fc5feddee2b55e1b0c6f2e8d2994
06-shuu <154021913+06-shuu@users.noreply.github.com>
2024-03-25 03:21:21 +0300

    vehicle purchase exercise - done

---
+++ cpp/Vehicle_Purchase/src/vehicle_purchase.cpp
@@ -0,0 +1,45 @@
+#include "vehicle_purchase.h"
+#include <iostream>
+namespace vehicle_purchase {
+
+    // needs_license determines whether a license is needed to drive a type of vehicle. Only "car" and "truck" require a license.
+    bool needs_license(std::string kind) {
+        // TODO: Return true if you need a license for that kind of vehicle.
+        if (kind == "car" || kind == "truck")
+            return true;
+        return false;
+    }
+
+    // choose_vehicle recommends a vehicle for selection. It always recommends the vehicle that comes first in lexicographical order.
+    std::string choose_vehicle(std::string option1, std::string option2) {
+        // TODO: Return the final decision in a sentence.
+        if (option1 < option2)
+            return option1 + " is clearly the better choice.";
+        else
+            return option2 + " is clearly the better choice.";
+    }
+
+    // calculate_resell_price calculates how much a vehicle can resell for at a certain age.
+    double calculate_resell_price(double original_price, double age) {
+        // TODO: Return the age-corrected resell price.
+        if (age < 3)
+            return original_price * 0.8;
+        else if (age >= 10)
+            return original_price * 0.5;
+        else 
+            return original_price * 0.7;
+    }
+
+}  // namespace vehicle_purchase
+
+//test
+int main() {
+
+   std::cout << vehicle_purchase::needs_license("car") << std::endl;
+   std::cout << vehicle_purchase::choose_vehicle("Wuling Hongguang", "Toyota Corolla")<< std::endl;
+   std::cout << vehicle_purchase::calculate_resell_price(1000, 1)<< std::endl;
+   std::cout << vehicle_purchase::calculate_resell_price(1000, 5) << std::endl;
+   std::cout << vehicle_purchase::calculate_resell_price(1000.0, 15) << std::endl;
+
+   return 0; 
+}
