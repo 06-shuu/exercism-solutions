ce1db379d323d3b0e5892884f561ff387010dd3a
06-shuu <154021913+06-shuu@users.noreply.github.com>
2024-03-29 02:38:25 +0300

    trinary exercise - done

---
+++ cpp/Trinary/src/trinary.cpp
@@ -0,0 +1,34 @@
+#include "trinary.h"
+
+
+namespace trinary {
+	int trinary::to_decimal(std::string trinary) {
+        int decimal{ 0 }, i{ 0 }, n{ static_cast<int>(trinary.length()) };
+
+        //reverse the order of the number
+        for (int i = 0; i < n / 2; i++) {
+            std::swap(trinary[i], trinary[n - i - 1]);
+        }
+
+        //convert it to decimal
+        for (auto num : trinary) {
+            if (num >= '0' && num <= '2') {
+                decimal += static_cast<int>((num - '0') * std::pow(3, i));
+                ++i;
+            }
+            else
+                return 0;
+        }
+        return decimal;
+	}
+}  // namespace trinary
+
+int main() {
+	std::string trinary1 = "1122000120";  
+	std::cout <<trinary::to_decimal(trinary1) << std::endl;
+
+    std::string trinary2 = "222";
+    std::cout << trinary::to_decimal(trinary2) << std::endl;
+	return 0;
+}
+
---
+++ cpp/Trinary/src/trinary.h
@@ -0,0 +1,12 @@
+#if !defined(TRINARY_H)
+#define TRINARY_H
+#include <iostream>
+#include <cmath>
+#include <string>
+
+namespace trinary {
+
+	int to_decimal(std::string trinary);
+}  // namespace trinary
+
+#endif // TRINARY_H
