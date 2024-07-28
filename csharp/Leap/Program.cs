3012c1bd13b17563aacf0db84b1ad58b619a7d17
06-shuu <154021913+06-shuu@users.noreply.github.com>
2024-07-28 15:10:01 +0300

    Leap year exercise - done :ant:

---
+++ csharp/Leap/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leap
+{
+    class Program
+    {
+
+        public static bool IsLeapYear(int year)
+        {
+
+            if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
+                return true;
+            return false;
+        } 
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsLeapYear(1999));
+        }
+    }
+}
